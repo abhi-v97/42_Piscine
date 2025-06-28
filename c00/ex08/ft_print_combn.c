2d9bfb9c1b74f99824fc64e86e196857ede76336
abhi-v97 <v.abhishek@outlook.com>
2025-06-28 22:20:30 +0100

    went back to and completed c00 ex08

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -27,13 +27,11 @@ void	print_num(char *c, int n)
 	if (digit == 0)
 	{
 		while (i < n)
+			ft_putchar(c[i++] + '0');
+		if (c[0] < 10 - n)
 		{
-			ft_putchar(c[i++] + 48);
-			if (c[0] < (10 - n))
-			{
-				ft_putchar(',');
-				ft_putchar(' ');
-			}
+			ft_putchar(',');
+			ft_putchar(' ');
 		}
 	}
 }
@@ -41,21 +39,39 @@ void	print_num(char *c, int n)
 void	ft_print_combn(int n)
 {
 	char	output[10];
-	int		i;
+	int		k;
 
-	i = 0;
-	while (i < n)
-		output[i++] = 0;
-	i = 0;
+	for (int i = 0; i < n; i++)
+		output[i] = i;
 	while (output[0] <= (10 - n))
 	{
-		print_num(output, n);
-		output[n - 1]++;
+		while (output[n - 1] < 10)
+		{
+			print_num(output, n);
+			output[n - 1]++;
+		}
+		if (output[0] == (10 - n) || output[0] > 9)
+			break ;
+        k = n - 2;
+        while (k >= 0 && output[k] == 10 - n + k)
+            k--;
+		output[k]++;
+        for (int j = k + 1; j < n; j++)
+            output[j] = output[j - 1] + 1;
 	}
+	ft_putchar('\n');
 }
-/*
-int	main(void)
+
+// TESTING: ./a.out n | sed 's/, /\n/g' | uniq -c  | wc -l
+// the result should be a number exactly equal to 10 choose n (eg 10 choose 3 = 120)
+// remove the wc -l to see that all output is unique
+int	main(int argc, char **argv)
 {
-	ft_print_combn(1);
+	if (argc < 2)
+	{
+		write(1, "Please provide an int argument 0 < n < 10\n", 32);
+		return (1);
+	}
+	ft_print_combn(*argv[1] - '0');
+	return (0);
 }
-*/
